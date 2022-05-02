0d0093588b22f84c00ca4171e28776cae75f0ccd
unknown <oscar.bravo@tismart.com>
2022-05-02 05:02:49 -0500

    changes for list item open modal

--- src/components/TableList/ListItem.vue
+++ src/components/TableList/ListItem.vue
@@ -2,9 +2,11 @@
   <div
     class="bg-white rounded-md py-2 px-3 w-full flex justify-between items-center"
   >
-    <span class="capitalize text-black-1 text-xl font-medium">{{
-      pokemon.name
-    }}</span>
+    <span
+      class="capitalize text-black-1 text-xl font-medium w-full"
+      @click="openModal(pokemon)"
+      >{{ pokemon.name }}</span
+    >
     <div
       class="rounded-full bg-app w-11 h-11 flex justify-center items-center text-2xl"
     >
@@ -43,6 +45,9 @@ export default {
         }
         return x;
       });
+    },
+    openModal(pokemon) {
+      console.log("open modal", pokemon);
     },
   },
 };
